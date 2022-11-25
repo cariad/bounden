1676c3192cb57fcb1577a9f9ebf6f08a5b7646ae
Cariad Eccleston <cariad@cariad.earth>
2022-11-25 16:11:59 +0000

    Add PointABC and PointT

--- bounden/points/types.py
+++ bounden/points/types.py
@@ -1,4 +1,5 @@
-from typing import Any, TypeVar
+from abc import ABC, abstractmethod
+from typing import Any, Generic, TypeVar
 
 AxesT = TypeVar("AxesT", bound=tuple[Any, ...])
 LengthsT = TypeVar("LengthsT", bound=tuple[float, ...])
@@ -11,3 +12,18 @@ YAxisT = TypeVar("YAxisT")
 
 XLengthT = TypeVar("XLengthT", bound=float)
 YLengthT = TypeVar("YLengthT", bound=float)
+
+
+class PointABC(ABC, Generic[AxesT]):
+    """
+    Abstract base point.
+    """
+
+    @abstractmethod
+    def __len__(self) -> int:
+        """
+        Gets the number of coordinates that describe this position.
+        """
+
+
+PointT = TypeVar("PointT", bound=PointABC[Any])
