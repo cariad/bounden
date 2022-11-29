6d56c015ecdab0d794cb3620d3ffcba9c3568629
Cariad Eccleston <cariad@cariad.earth>
2022-11-29 09:24:33 +0000

    Clean up

--- bounden/__init__.py
+++ bounden/__init__.py
@@ -6,6 +6,11 @@ For usage and support, see https://github.com/cariad/bounden.
 
 from importlib.resources import files
 
+from bounden.coordinates import (
+    FloatCoordinate,
+    IntegerCoordinate,
+    StringCoordinate,
+)
 from bounden.points import Point, Point2
 from bounden.regions import Region, Region2
 from bounden.vectors import Vector, Vector2
@@ -22,10 +27,13 @@ def version() -> str:
 
 
 __all__ = [
+    "FloatCoordinate",
+    "IntegerCoordinate",
     "Point",
     "Point2",
     "Region",
     "Region2",
+    "StringCoordinate",
     "Vector",
     "Vector2",
     "Volume",
